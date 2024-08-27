d6276ccada0f84c3672d3d73cca16e9c2b7dec6a
chenlong <chenlong1@jkinvest.cn>
2024-08-27 14:25:25 +0800

    add husky

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-let a = 100
+const str = 'abc'
+const str2 = "def"
+console.log(str, str2)
 </script>
 
 <template>
@@ -13,16 +15,18 @@ let a = 100
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .logo {
   height: 6em;
   padding: 1.5em;
-  will-change: filter;
   transition: filter 300ms;
+  will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
