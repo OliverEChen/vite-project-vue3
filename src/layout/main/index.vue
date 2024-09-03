1d30d69b4b460657a2e68b5f8bf72da7eb6b6b04
chenlong <chenlong1@jkinvest.cn>
2024-09-03 16:51:48 +0800

    style: update

--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view v-slot="{ Component }">
-    <transition name="fade">
+    <transition name="slide-fade" mode="out-in">
       <component :is="Component"></component>
     </transition>
   </router-view>
@@ -8,13 +8,17 @@
 
 <script setup lang="ts"></script>
 <style scoped lang="scss">
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
 }
 
-.fade-enter-from,
-.fade-leave-to {
+.slide-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
   opacity: 0;
 }
 </style>
